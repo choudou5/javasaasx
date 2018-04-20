fcefc9ccae0292caaad87c48efc45628de97a55c
haowen <haowen@lianj.com>
2018-04-20 11:34:23 +0800

    排除 zkclient 的 slf4j-log4j12

--- javasaasx.impl/lib/mvn-install.bat
+++ javasaasx.impl/lib/mvn-install.bat
@@ -1,2 +1,2 @@
-mvn install:install-file -Dfile=D:\workspace-idea-java\javasaasx\javasaasx.impl\lib\client-sdk.api-1.0.2.jar -DgroupId=com.dingtalk.open -DartifactId=client-sdk.api -Dversion=1.0.2 -Dpackaging=jar
+mvn install:install-file -Dfile=D:\workspace-demo\javasaasx\javasaasx.impl\lib\client-sdk.api-1.0.2.jar -DgroupId=com.dingtalk.open -DartifactId=client-sdk.api -Dversion=1.0.2 -Dpackaging=jar
 
